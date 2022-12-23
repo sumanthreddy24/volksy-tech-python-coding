baaa3ad54ba6b3c30dd24f42eb59f5f0b41cce19
sumanthreddy24 <sumantht@brightcomgroup.com>
2022-12-23 11:22:36 +0000

    second

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "HOLBERTON"
-print("First three letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[:2]))
-print("Middle word: {}".format(word[1:8]))
+word_first_3 = word[:3]
+word_first_2 = word[:2]
+middle_word = word[1:8]
+print("First three letters: {}".format(word_first_3]))
+print("Last 2 letters: {}".format(word_first_2))
+print("Middle word: {}".format(middle_word))
