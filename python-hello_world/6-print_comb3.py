eaccc4f32afc9f18cf597d5fbee464ef6b138b38
sumanthreddy24 <sumantht@brightcomgroup.com>
2022-12-26 11:24:14 +0000

    ne file

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i == 8 and j == 9:
-           print("89")
-        else i < j :
-           print("{}{}". format(i,j) end=", " )
+            print("89")
+        elif i < j:
+            print("{:d}{:d}" .format(i, j), end=", ")
